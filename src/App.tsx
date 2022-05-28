30cf782f25e7222132f82b804209eff3ca63c2be
Muhammad Nur Faisi S <faizi1234098@gmail.com>
2022-05-28 16:05:50 +0800

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { Login } from './pages/Login';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
